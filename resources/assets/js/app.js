562e8b4ad1969eba9af08e42206852a858ab6b7a
Mohamad Sholihin <mohamad.sholihin.it@gmail.com>
2015-12-13 07:47:59 +0700

    salah konsep assets jadi di hapus

--- resources/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.edit').click( function() {
-         $.get( $(this).attr('href'), function(msg) {
-            console.log('data : '+[msg]);
-         });
-         return false; // don't follow the link!
-    });
-});
